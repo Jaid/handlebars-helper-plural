1d74061b2e349d68610d8b3872932bae3b42a5fc
Jaid <jaid.jsx@gmail.com>
2019-07-07 19:26:02 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 /** @module handlebars-helper-plural */
 
+import {last, dropRight, isString} from "lodash"
+import pluralize from "pluralize"
+
 /**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
+ * Handlebars helper that returns given number with a possibly pluralized noun
  * @function
- * @returns {number} Seconds since epoch
+ * @returns {string} Compiled text
  * @example
+ * import Handlebars from "handlebars"
  * import handlebarsHelperPlural from "handlebars-helper-plural"
- * const result = handlebarsHelperPlural()
- * result === 1549410770
+ * const handlebars = Handlebars.create()
+ * handlebars.registerHelper("plural", handlebarsHelperPlural)
+ * const template = handlebars.compile("I have {{plural bananas 'banana' 'bananas'}}!")
+ * const result = template({bananas: 4})
+ * result === "I have 4 bananas!"
  */
-export default () => Math.floor(Date.now() / 1000)
+export default (...args) => {
+  const {data, hash} = args |> last
+  const [count, singularNoun, extraArgument] = args |> dropRight
+  const getNumberString = () => {
+    if (count === 1) {
+      if (hash.one === true) {
+        return "one"
+      }
+      if (hash.one |> isString) {
+        return hash.one
+      }
+    }
+    return count
+  }
+  const getPluralNoun = () => {
+    return hash.plural || extraArgument || pluralize(singularNoun)
+  }
+  const string = `${getNumberString()} ${count === 1 ? singularNoun : getPluralNoun()}`
+  debugger
+  return string
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 import path from "path"
 
+import Handlebars from "handlebars"
+
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
 /**
@@ -7,7 +9,17 @@ const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.jo
    */
 const {default: handlebarsHelperPlural} = indexModule
 
-it("should run", () => {
-  const result = handlebarsHelperPlural()
-  expect(result).toBeGreaterThan(1549410770)
+const handlebars = Handlebars.create()
+handlebars.registerHelper("plural", handlebarsHelperPlural)
+
+it("should run for singular", () => {
+  const template = handlebars.compile("I have {{plural bananas 'banana' one=true}}!")
+  const result = template({bananas: 1})
+  expect(result).toBe("I have one banana!")
+})
+
+it("should run for plural", () => {
+  const template = handlebars.compile("I have {{plural bananas 'banana' 'bananas'}}!")
+  const result = template({bananas: 4})
+  expect(result).toBe("I have 4 bananas!")
 })
